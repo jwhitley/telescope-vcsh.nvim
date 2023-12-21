f2b8efb611327f7329df8a75c1f712e9638c08d5
John Whitley <john@luminous-studios.com>
2023-12-21 15:11:17 -0800

    Initial working version

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # telescope-vcsh.nvim
+
+A minimal [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim/)
+plugin to list homedir files managed by 
+[vcsh](https://github.com/RichiH/vcsh).
+
---
+++ lua/telescope/_extensions/vcsh.lua
@@ -0,0 +1,20 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+
+local vcshFiles = function(opts)
+	opts = opts or {}
+	pickers
+		.new(opts, {
+			prompt_title = "Find Files in vcsh",
+			finder = finders.new_oneshot_job({ "vcsh-ls-files" }, opts),
+			sorter = conf.file_sorter(opts),
+		})
+		:find()
+end
+
+return require("telescope").register_extension({
+	exports = {
+		vcsh = vcshFiles,
+	},
+})
